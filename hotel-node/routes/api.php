5a38de1dfa918ff4f2b5d1d5a4809cc443b2a376
cotcute <dangduykhanh10a4@gmail.com>
2026-04-01 17:34:58 +0700

    Dọn rác của Ngọc và Kiên, tạo thư mục chuẩn cho cả team

--- hotel-node/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NodeController;
-
-Route::post('/can-commit', [NodeController::class, 'canCommit']);
-Route::post('/pre-commit', [NodeController::class, 'preCommit']);
-Route::post('/do-commit',  [NodeController::class, 'doCommit']);
-Route::post('/abort',      [NodeController::class, 'abort']);
